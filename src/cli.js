2e6bfe71cbf4c7f83f8a8a0c5e208cec4960f5fc
jamesgeorge007 <jamesgeorge998001@gmail.com>
2020-04-08 19:04:00 +0530

    chore: add usage info

--- src/cli.js
+++ src/cli.js
@@ -5,5 +5,8 @@ const { version } = require('../package');
 // register version flag
 program.version(version);
 
+// define usage information
+// program.usage('<command> [options]')
+
 // parse args
 program.parse(process.argv);
