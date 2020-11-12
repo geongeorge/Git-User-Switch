57496d5dd3f42440f79906e8c23208dbb127ad5f
Spenser Black <spenserblack01@gmail.com>
2020-11-12 14:04:43 -0500

    Set and clear signing key

--- src/lib/selectUser.js
+++ src/lib/selectUser.js
@@ -49,6 +49,24 @@ async function selectUser(users, isGlobal = false) {
   await execa
     .command(`git config ${globalFlag} user.email ${userEmail}`)
     .stdout.pipe(process.stdout);
+
+  if(user.signingKey) {
+    console.log(`Setting ${user.signingKey} as user.signingKey`);
+    await execa
+      .command(`git config ${globalFlag} user.signingKey ${user.signingKey}`)
+      .stdout.pipe(process.stdout);
+  } else if (
+    (await execa.command(`git config ${globalFlag} user.signingKey`)).stdout
+  ) {
+    console.log('Clearing user.signingKey');
+    const clearSigningKeyCommands = ['config', 'user.signingKey', ''];
+    if (globalFlag) {
+      clearSigningKeyCommands.splice(1, 0, globalFlag);
+    }
+    await execa('git', clearSigningKeyCommands)
+      .stdout.pipe(process.stdout);
+  }
+
   console.log("Done!");
 }
 function escapeString(str) {
