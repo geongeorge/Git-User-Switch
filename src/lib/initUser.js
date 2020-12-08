d25ecdc09d8fd30664be50e1dd8a45a64498e8a0
beet <63141491+beetcb@users.noreply.github.com>
2020-12-08 16:49:37 +0800

    feat: complete user init function

---
+++ src/lib/initUser.js
@@ -0,0 +1,32 @@
+/** @format */
+
+const execa = require("execa");
+
+async function initUser(store) {
+  const confList = (await execa.command("git config --list")).stdout.match(
+    /(?:(user.(name|email)))(.+)/g
+  );
+  const users = [];
+
+  // Count name,email,signKey to match multi user
+  let [nameC, emailC, signKeyC] = [0, 0, 0];
+  confList.forEach((e) => {
+    users[Math.max(nameC, emailC, signKeyC)] = {};
+    switch (e) {
+      case e.includes("name") ? e : null:
+        users[nameC++].name = e.slice(e.indexOf("=") + 1);
+        break;
+      case e.includes("email") ? e : null:
+        users[emailC++].email = e.slice(e.indexOf("=") + 1);
+        break;
+      case e.includes("signingKey") ? e : null:
+        users[signKeyC++].signingKey = e.slice(e.indexOf("=") + 1);
+        break;
+    }
+  });
+
+  users.pop();
+  store.set("users", users);
+}
+
+module.exports = initUser;
